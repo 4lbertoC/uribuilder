a5fac16fcdf027eef5c06bd4e68656c314a33802
Alberto Congiu <4lbertoc@gmail.com>
2012-11-11 20:47:23 +0100

    Add event handling for Url

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,6 +5,7 @@ goog.require('goog.dom');
 goog.require('goog.events');
 goog.require('urlbuilder.EventHandler');
 goog.require('urlbuilder.UI');
+goog.require('urlbuilder.UrlEvent');
 
 /**
  * The controller class.
@@ -20,39 +21,77 @@ urlbuilder.Controller = function()
 urlbuilder.Controller.prototype.init = function()
 {
   this.initEventHandler_();
-};
-
-urlbuilder.Controller.prototype.initEventHandler_ = function()
-{
-  var elements = goog.dom.getElementsByClass(urlbuilder.UI.TEXTFIELD_CLASSNAME);
-  var actions = {};
-  actions[urlbuilder.EventHandler.EventType.URL] = urlbuilder.Controller.parseUrl_;
-  actions[urlbuilder.EventHandler.EventType.FIELD] = urlbuilder.Controller.composeUrl_;
-  this.eventHandler_ = new urlbuilder.EventHandler();
-  this.eventHandler_.init(elements, actions);
+  this.initActions_();
 };
 
 /**
- * Parses a url and splits it into its components.
- * @param {string} url The input url.
+ * Links the events dispatched by the EventHandler
+ * to the Controller functions.
+ * @private
  */
-urlbuilder.Controller.parseUrl_ = function(url)
+urlbuilder.Controller.prototype.initActions_ = function()
 {
-  alert('ciao');
+  this.eventHandler_.addEventListener(
+	urlbuilder.EventHandler.EventType.URL, this.onUrlEvent_);
+  this.eventHandler_.addEventListener(
+	urlbuilder.EventHandler.EventType.FIELD, this.onFieldEvent_);
+};
+
+/**
+ * Activates the DOM elements listeners.
+ * @private
+ */
+urlbuilder.Controller.prototype.initEventHandler_ = function()
+{
+  var elements = goog.dom.getElementsByClass(urlbuilder.UI.TEXTFIELD_CLASSNAME);
+  this.eventHandler_ = new urlbuilder.EventHandler();
+  this.eventHandler_.init(elements);
 };
 
 /**
  * Composes the url into the url text field from the
  * data contained in the other fields.
  * @param {string} url The input url.
+ * @private
  */
 urlbuilder.Controller.composeUrl_ = function()
 {
-
+  // TODO
 };
+
+/**
+ * Parses a url and splits it into its components.
+ * @param {string} url The input url.
+ * @private
+ */
+urlbuilder.Controller.parseUrl_ = function(url)
+{
+  // TODO
+};
+
+/**
+ * Handles a Url event.
+ * @param {urlbuilder.UrlEvent} evt The Url event.
+ * @private
+ */
+urlbuilder.Controller.prototype.onUrlEvent_ = function(evt)
+{
+  var url = evt.url;
+  urlbuilder.Controller.parseUrl_(url);
+};
+
+/**
+ * Handles the event of field modification.
+ * @param {goog.events.Event} evt The event.
+ * @private
+ */
+urlbuilder.Controller.prototype.onFieldEvent_ = function(evt)
+{};
 
 /**
  * The event handler instance.
  * @type {urlbuilder.EventHandler}
+ * @private
  */
 urlbuilder.Controller.prototype.eventHandler_ = null;
+
