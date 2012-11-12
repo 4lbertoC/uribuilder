cbdc9955a7dc2ddbc59ead9df2ca1ead1fcd8559
Alberto Congiu <4lbertoc@gmail.com>
2012-11-12 23:02:45 +0100

    Add prod build mode

--- build.sh
+++ build.sh
@@ -22,54 +22,60 @@
 #
 if [ $1 ]; then
   if [ $1 == "dev" -o $1 == "prod" ]; then
-    BUILDMODE=$1
+    BUILD_MODE=$1
   fi
 fi
 
 #
 # Clean
 #
-/bin/rm -rf $BUILDDIR
+rm -rf $BUILD_DIR
 
 #
 # Create build directory
 #
-/bin/mkdir -p $BUILDDIR
+mkdir -p $BUILD_DIR
 
 #
 # Dev build
 #
-if [ $BUILDMODE == "dev" ]; then
+if [ $BUILD_MODE == "dev" ]; then
   echo "Building development package..."
   
   # Escape Closure dir to be used in sed
-  SAFECLOSUREDIR=$(printf "%s\n" "$CLOSUREDIR" | /bin/sed 's/[][\.*^$/]/\\&/g')
+  SAFE_CLOSURE_LIBRARY_PATH=$(printf "%s\n" "$CLOSURE_LIBRARY_PATH" | sed 's/[][\.*^$/]/\\&/g')
   # Prefix to append to each dependency path 
-  PREFIX="../../../../"$PATH
+  PREFIX="../../../../"$SCRIPTS_PATH
   # Create deps file
-  $PYTHON $DEPSWRITERCMD --root_with_prefix=$PATH" "$PREFIX > $BUILDDIR/$DEPSFILENAME
+  $PYTHON $DEPSWRITER_CMD --root_with_prefix=$SCRIPTS_PATH" "$PREFIX > $BUILD_DIR/$DEPS_FILENAME
   # Remove <!--PROD PROD--> into index.html
-  /bin/sed -e :a -re 's/<!--PROD.*?PROD-->//g;/<!--/N;//ba' < index.html | 
+  sed -e :a -re 's/<!--PROD.*?PROD-->//g;/<!--/N;//ba' < index.html | 
   # Uncomment <!--DEV DEV--> into index.html
-  /bin/sed -e :a -re 's/<!--DEV(.*?)DEV-->/\1/g;/<!--/N;//ba' |
+  sed -e :a -re 's/<!--DEV(.*?)DEV-->/\1/g;/<!--/N;//ba' |
   # Replace Closure Library directory
-  /bin/sed -e 's/\${CLOSUREDIR}/..\/'$SAFECLOSUREDIR'/g' > $BUILDDIR/index.html
+  sed -e 's/\${CLOSURE_LIBRARY_PATH}/..\/'$SAFE_CLOSURE_LIBRARY_PATH'/g' > $BUILD_DIR/index.html
 
 #
 # Prod build
 #
-elif [ $BUILDMODE == "prod" ]; then
+elif [ $BUILD_MODE == "prod" ]; then
   echo "Building production package..."
   # Generate compiled file into build dir
-  # TODO
+  $PYTHON $CLOSUREBUILDER_CMD --root=$CLOSURE_LIBRARY_PATH/ \
+                             --root=$SCRIPTS_PATH/ \
+                             --namespace="urlbuilder" \
+                             --output_mode=compiled \
+                             --compiler_jar=$CLOSURE_COMPILER_PATH/compiler.jar \
+                             --compiler_flags="--compilation_level=$COMPILATION_LEVEL" \
+                             > $BUILD_DIR/urlbuilder.min.js
   # Remove <!--DEV DEV--> into index.html
-  /bin/sed -e :a -re 's/<!--DEV.*?DEV-->//g;/<!--/N;//ba' < index.html |
+  sed -e :a -re 's/<!--DEV.*?DEV-->//g;/<!--/N;//ba' < index.html |
   # Uncomment <!--PROD PROD--> into index.html
-  /bin/sed -e :a -re 's/<!--PROD(.*?)PROD-->/\1/g;/<!--/N;//ba' > $BUILDDIR/index.html
+  sed -e :a -re 's/<!--PROD(.*?)PROD-->/\1/g;/<!--/N;//ba' > $BUILD_DIR/index.html
 fi
 
 # Copy style
-/bin/cp -r style $BUILDDIR
+cp -r style $BUILD_DIR
 
 echo "Done."
 
--- config.sh
+++ config.sh
@@ -4,36 +4,44 @@
 # URLBUILDER VARS
 #################
 
-# Path to the urlbuilder directory relative to the Closure Library [DEV-ONLY]
-PATH="../urlbuilder/scripts"
+# Path to the js scripts directory relative to the Closure Library [DEV-ONLY]
+SCRIPTS_PATH="../urlbuilder/scripts"
 
 # Output filename [DEV-ONLY]
-DEPSFILENAME="urlbuilder_deps.js"
+DEPS_FILENAME="urlbuilder_deps.js"
 
 #################
 # BUILD OPTIONS
 #################
 
 # Output dir
-BUILDDIR="build"
+BUILD_DIR="build"
 
 # Build mode (dev|prod)
-BUILDMODE="prod"
+BUILD_MODE="prod"
 
 #################
 # PYTHON VARS
 #################
 
-# Directory in which Python is installed
+# Path to Python command
 PYTHON="/usr/bin/python"
 
 #################
 # CLOSURE VARS
 #################
 
-# Directory in which the Closure Library is installed
-CLOSUREDIR="../extlib/"
+# Path in which the Closure Library is installed
+CLOSURE_LIBRARY_PATH="../extlib/closure-library/"
+
+# Path in which the Closure Compiler is installed
+CLOSURE_COMPILER_PATH="../extlib/closure-compiler/"
 
 # Command to execute to create deps file [DEV-ONLY]
-DEPSWRITERCMD=$CLOSUREDIR"closure-library/closure/bin/build/depswriter.py"
+DEPSWRITER_CMD=$CLOSURE_LIBRARY_PATH"closure/bin/build/depswriter.py"
 
+# Command to execute to create deps file [PROD-ONLY]
+CLOSUREBUILDER_CMD=$CLOSURE_LIBRARY_PATH"closure/bin/build/closurebuilder.py"
+
+# Compilation level
+COMPILATION_LEVEL="ADVANCED_OPTIMIZATIONS"
